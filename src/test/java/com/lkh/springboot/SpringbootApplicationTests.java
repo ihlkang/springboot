231e5f11de11c96d7255b0d84fbdce77775debd7
ihlkang <361059351@qq.com>
2018-08-02 14:49:12 +0800

    mybatis与数据库交互

--- src/test/java/com/lkh/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/lkh/springboot/SpringbootApplicationTests.java
@@ -9,8 +9,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SpringbootApplicationTests {
 
+ 
     @Test
     public void contextLoads() {
+        
     }
 
 }
